f58d82e1b85c18c084285f5fec23dd9ff2c1484e
Sergei US <ustimenko.sv@gmail.com>
2022-09-19 18:41:19 +0300

    Initial commit

--- src/test/java/ru/netology/MyAutomation3/OrderTestPositive.java
+++ src/test/java/ru/netology/MyAutomation3/OrderTestPositive.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -28,12 +29,12 @@ class OrderTestPositive {
     @BeforeEach
     void setUp() {
 
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-//        driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver();
     }
 
     @AfterEach
